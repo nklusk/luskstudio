e74c913b3c813933c2364b527342f29f496b5aeb
NATALIE LUSK <NKL22@fsu.edu>
2025-03-01 15:31:22 -0500

    images loading in squares

--- sketch.js
+++ sketch.js
@@ -4,13 +4,23 @@ let thumbnailsLoaded = 0
 
 function preload() {
   for (let i = 0; i < 2; i++){
-    loadImage('assets/' +thumbnailFiles[i]);
+    loadImage('assets/' +thumbnailFiles[i], imageLoaded);
   }
+}
+
+function imageLoaded(img) {
+    thumbnails.push(img);
+    thumbnailsLoaded++;
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textFont('Helvetica');
+
+  let newWidth = 150; // Set your desired width here
+  for (let i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].resize(newWidth, 0);
+  }
 }
 
 
@@ -23,6 +33,13 @@ function draw() {
     let squareXcoord = (((windowWidth%150)/2+75)+(i*150));
     let squareYcoord = 200
     square(squareXcoord, squareYcoord, 150);
+
+  }
+
+  for (let i = 0; i < thumbnails.length; i++) {
+    let imageXcoord = (((windowWidth%150)/2+150)+(i*150));
+    let imageYcoord = 275
+    image(thumbnails[i], imageXcoord, imageYcoord);
   }
 
 }
