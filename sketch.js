bf6ab6cf09e761b1c1cd1fd9c8bd522f865983de
NATALIE LUSK <NKL22@fsu.edu>
2025-03-01 14:43:24 -0500

    started drawing grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let thumbnails = [];
+let numThumbnails = 2;
+
+// function preload() {
+//   for (let i = 0; i < numThumbnails; i++){
+//     thumbnails[i] = loadImage('/assets/thumbnail${i}.png');
+//   }
+// }
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  textFont('Helvetica');
 }
+
 
 function draw() {
   background(125);
+  console.log(displayWidth*pixelDensity())
+  for (let i = 0; i < numThumbnails; i++){
+
+    let squareXcoord = (((windowWidth%20)/2)+(i*20));
+    let squareYcoord = 40
+    square(squareXcoord, squareYcoord, 20);
+  }
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
